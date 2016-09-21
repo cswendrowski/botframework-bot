ca64ca02a32ad5b905ff54948b4d6329bbb9aa5e
Cody Swendrowski <cody@swendrowski.us>
2016-09-20 22:55:30 -0500

    Adds rule that checks for SCSS rules like margin: 0 0 0 0;

---
+++ RezaBot/BotTests/Rules/Sitecore/Scss0000Tests.cs
@@ -0,0 +1,47 @@
+﻿using BotTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RezaBot.Rules.Sitecore;
+
+namespace BotTests.Rules.Sitecore
+{
+    [TestClass]
+    public class Scss0000Tests
+    {
+        [TestMethod]
+        public void Exists()
+        {
+            var rule = GetRule();
+
+            Assert.IsNotNull(rule);
+        }
+
+        [TestMethod]
+        public void FindsExtra0s()
+        {
+            var file = TestHelpers.CreateTestFile("scss", "+ margin: 0 0 0 0;");
+
+            GetRule().AssertFoundIssue(file, file.ChangedLines);
+
+            file = TestHelpers.CreateTestFile("scss", "+ padding: 0 0 0;");
+
+            GetRule().AssertFoundIssue(file, file.ChangedLines);
+
+            file = TestHelpers.CreateTestFile("scss", "+ margin: 0 0;");
+
+            GetRule().AssertFoundIssue(file, file.ChangedLines);
+        }
+
+        [TestMethod]
+        public void OnlyOne0()
+        {
+            var file = TestHelpers.CreateTestFile("scss", "+ margin: 0;");
+
+            GetRule().AssertIsIssueFree(file, file.ChangedLines);
+        }
+
+        private Scss0000 GetRule()
+        {
+            return TestHelpers.GetRule<Scss0000>();
+        }
+    }
+}
---
+++ RezaBot/RezaBot/Rules/Sitecore/Scss0000.cs
@@ -0,0 +1,59 @@
+﻿using PullRequestReviewService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RezaBot.Rules.Sitecore
+{
+    public class Scss0000 : SitecoreBaseRule
+    {
+        protected override List<CodeComment> Review(ChangedFile file, List<CodeLine> addedLines, out bool issueFound)
+        {
+            var messages = new List<CodeComment>();
+            issueFound = false;
+
+            if (file.FileType == "scss")
+            {
+                foreach (var changedLine in addedLines)
+                {
+                    if (changedLine.Line.Contains("0 0 0 0"))
+                    {
+                        messages.Add(new CodeComment
+                        {
+                            File = file,
+                            Line = changedLine,
+                            Comment = "Please simplify this rule from `0 0 0 0` down to `0`, thanks!"
+                        });
+
+                        issueFound = true;
+                    }
+                    else if (changedLine.Line.Contains("0 0 0"))
+                    {
+                        messages.Add(new CodeComment
+                        {
+                            File = file,
+                            Line = changedLine,
+                            Comment = "Please simplify this rule from `0 0 0` down to `0`, thanks!"
+                        });
+
+                        issueFound = true;
+                    }
+                    else if (changedLine.Line.Contains("0 0"))
+                    {
+                        messages.Add(new CodeComment
+                        {
+                            File = file,
+                            Line = changedLine,
+                            Comment = "Please simplify this rule from `0 0` down to `0`, thanks!"
+                        });
+
+                        issueFound = true;
+                    }
+                }
+            }
+
+            return messages;
+        }
+    }
+}
