51e1ad9c46b654fb2b42332e24fd82cbf32e3d78
Cody Swendrowski <cody@swendrowski.us>
2016-09-21 00:47:30 -0500

    Pulls generic Evaluate logic into Rule class

--- RezaBot/PullRequestReviewService/Models/Rule.cs
+++ RezaBot/PullRequestReviewService/Models/Rule.cs
@@ -1,5 +1,6 @@
 ﻿using PullRequestReviewService.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PullRequestReviewService.Models
 {
@@ -9,6 +10,26 @@ namespace PullRequestReviewService.Models
 
         protected List<string> FileTypesToCheck = new List<string>();
 
-        public abstract List<CodeComment> Evaluate(ChangedFile file, List<CodeLine> addedLines, out bool issueFound);
+        public List<CodeComment> Evaluate(ChangedFile file, List<CodeLine> addedLines, out bool issueFound)
+        {
+            var messages = new List<CodeComment>();
+
+            var fileTypes = FileTypesToCheck.Select(x => x.ToLower());
+
+            if (!FileNamesToIgnore.Contains(file.FileName) &&
+                (fileTypes.Contains("any") || fileTypes.Contains(file.FileType)))
+            {
+                messages.AddRange(Review(file, addedLines, out issueFound));
+
+                return messages;
+            }
+
+            // We didn't review the file, so no issues were found
+            issueFound = false;
+
+            return messages;
+        }
+
+        protected abstract List<CodeComment> Review(ChangedFile file, List<CodeLine> addedLines, out bool issueFound);
     }
 }
--- RezaBot/RezaBot/Rules/Sitecore/SitecoreBaseRule.cs
+++ RezaBot/RezaBot/Rules/Sitecore/SitecoreBaseRule.cs
@@ -1,6 +1,4 @@
 ﻿using PullRequestReviewService.Models;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace RezaBot.Rules
 {
@@ -10,27 +8,5 @@ namespace RezaBot.Rules
         {
             FileNamesToIgnore.Add("GlassItems.cs");
         }
-
-        public override List<CodeComment> Evaluate(ChangedFile file, List<CodeLine> addedLines, out bool issueFound)
-        {
-            var messages = new List<CodeComment>();
-
-            var fileTypes = FileTypesToCheck.Select(x => x.ToLower());
-
-            if (!FileNamesToIgnore.Contains(file.FileName) &&
-                (fileTypes.Contains("any") || fileTypes.Contains(file.FileType)))
-            {
-                messages.AddRange(Review(file, addedLines, out issueFound));
-
-                return messages;
-            }
-
-            // We didn't review the file, so no issues were found
-            issueFound = false;
-
-            return messages;
-        }
-
-        protected abstract List<CodeComment> Review(ChangedFile file, List<CodeLine> addedLines, out bool issueFound);
     }
 }
